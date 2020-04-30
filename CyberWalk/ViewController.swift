44b5485092524d0b071ba2d7a82aaceb33334812
Kefan Zhang <939149050@qq.com>
2020-04-30 00:27:02 -0400

    update

--- CyberWalk/ViewController.swift
+++ CyberWalk/ViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 import SceneKit
 import ARKit
+import SpriteKit
+
+var flag = false
+
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
+    var isAquireTouched = false
+    
     @IBOutlet var sceneView: ARSCNView!
+    private lazy var connectButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .blue
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle(NSLocalizedString("Personal Profile", comment: ""), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.sizeToFit()
+        button.frame.origin = CGPoint(x: 0, y: 800)
+        button.frame = CGRect(x:0, y:600, width:400, height:50)
+        button.backgroundColor = UIColor(red: 0.4, green: 1.0, blue: 0.2, alpha: 0.5)
+
+        return button
+    }()
+    private lazy var resButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.lineBreakMode = .byWordWrapping
+        button.titleLabel?.textAlignment = .center
+        button.setTitle(NSLocalizedString("Object Aquired!\n go check", comment: ""), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Verdana", size: 40)
+        //button.titleLabel.lineBreakMode = UILineBreakModeWordWrap;
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.sizeToFit()
+        button.frame.origin = CGPoint(x: 0, y: 800)
+        button.frame = CGRect(x:0, y:0, width:400, height:400)
+        button.backgroundColor = UIColor(red: 0, green: 1.0, blue: 0.2, alpha: 0.5)
+
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +63,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        sceneView.addSubview(connectButton)
+        if(flag){
+            sceneView.addSubview(resButton)
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -50,15 +90,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let t:UITouch = touch as! UITouch
+            print("first touch, flag is: " + flag.description)
+            flag = true
+            print("changed, now flag is: " + flag.description)
+            self.viewDidLoad()
+            
+        }
+    }
+    
     // MARK: - ARSCNViewDelegate
     
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         
     
         let node = SCNNode()
-        
         if let objectAnchor = anchor as? ARObjectAnchor {
-
+            print("going through if condition, now bool is:" + isAquireTouched.description)
             let plane = SCNPlane(width: CGFloat(objectAnchor.referenceObject.extent.x * 3), height: CGFloat(objectAnchor.referenceObject.extent.y * 1.5))
             
             plane.cornerRadius = plane.width / 8
@@ -75,15 +125,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             node.addChildNode(planeNode)
             
             
+            //let plane2 = SCNPlane(width: CGFloat(objectAnchor.referenceObject.extent.x * 3), height: CGFloat(objectAnchor.referenceObject.extent.y * 1.5))
+            
+            //plane2.cornerRadius = plane2.width / 8
+            //let spriteKitScene2 = SKScene(fileNamed: "touchedScene")
+            
+            //plane2.firstMaterial?.diffuse.contents = spriteKitScene2
+            //plane2.firstMaterial?.isDoubleSided = true
+            //plane2.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
+            
+            //let planeNode2 = SCNNode(geometry: plane2)
+            //planeNode2.position = SCNVector3Make(objectAnchor.referenceObject.center.x + 0.01, objectAnchor.referenceObject.center.y + 0.25, objectAnchor.referenceObject.center.z)
+            
+            
         }
         
         return node
     }
     
-    func buttonTap() {
-        print("Button pressed")
-    }
-
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -99,5 +158,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
 
 }
--- README.md
+++ README.md
@@ -1,52 +1,7 @@
-# EC500_TriangleProject
+# ARKit 2 Tutorial: Create an AR Shopping Experience - Scan & Detect Real 3D Objects
 
-## Introduction to the Game:
+Learn how to scan real world objects to detect them in your app with ARKit and create a stunning AR shopping experience.
 
-We would like to explore the possiblity combining **Augmented Reality** (AR) and **Object Detection** technologies and present them with a game, which encourages players to go outside (certainly not now, stay home!), explore certain **targets** as symbols to acquire **materials** from. 
+➡️ [Video on YouTube](https://youtu.be/FEqBW3cKF2k)
 
-With those materials (e.g. metal, wood and so on), players are able to upgrade their **reward**, which is a product displayed in AR way (which is really cool to show friends, right?) and owned by each user. For example, with upgrading, you can upgrade your little wood house into a huge spectacular castle, also we hope we have enough time to finish social functions, which allows people to check on their friends' rewards.
-
-## User Story:
-- As a **game player**, I want to combine online games with real life. I not only can play games on the phone, but also can take physical activities outside.
-- As a **sponsor**, I want to display my product in game to attract more customers.
-
-
-
-
-## Minimum Valuable Product: 
-
-- **Object Detection**
-
-  - Recognize the objects and classify them by labeling. For example, if we use a camera to scan the wooden chair, and it will return what it is and label it with wood.
-
-- **Profile page**
-  - This is a visualization page for users to see what resources and the amount they have collected. Besides, they can use these resources to build their own architecture.
-
-- **AR**
-  - When users start their camera and point to the open area, the architecture will display here by AR.
-
-## Tech: 
-- Object Detection
-- Augmented Reality, Unity
-- Swift, C#, Python
-- AWS
-
-
-# Sprint 1: 
-
-## Taskboard:
-
-- Determine details of the game 
-- Display basic (level 1) reward architecture
-- Connect Object Detection
-- Have at least one sample conversion from acquired object to materials
-- \*Login/logout
-
-# Sprint 2:
-
-## Taskboard:
-
-- Deployment on iOS
-- Other pages in app
-- Connect with Object Detection 
-- \*Server Configuration
+[![Video](https://img.youtube.com/vi/FEqBW3cKF2k/0.jpg)](https://www.youtube.com/watch?v=FEqBW3cKF2k)
